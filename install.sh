6fd6fad9b59a73a003d104c11ec95f86356196e5
Michael Beasley <youvegotmoxie@gmail.com>
2015-07-29 03:12:01 -0400

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 PATH="${PATH}
-
+if [ uid != 0 ] then;
+	exit
+else
 pkg update
 pkg install puppet
 echo 'puppet_enable="YES"' >> /etc/rc.conf
 echo 'puppet_flags="-v --listen --server fbsd-srv02.servebeer.info"
-
+cp ~/puppet-bootstrap/configs/auth.conf /usr/local/etc/puppet/auth.conf
+puppet agent -v --server fbsd-srv02.servebeer.info --waitforcert 60 --test
+service puppet start
+fi
+exit 0
