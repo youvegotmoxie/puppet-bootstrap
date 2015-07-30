5b0c0f86dd44e77a7b475e8849b0f278bdadc6d3
Michael Beasley <youvegotmoxie@gmail.com>
2015-07-30 13:12:04 -0400

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,31 @@
 #!/bin/sh
 PATH="${PATH}"
-if [ uid == 0 ]; then
-pkg update
-pkg install puppet
-echo 'puppet_enable="YES"' >> /etc/rc.conf
-echo 'puppet_flags="-v --listen --server fbsd-srv02.servebeer.info"' >> /etc/rc.conf
-cp ~/puppet-bootstrap/configs/auth.conf /usr/local/etc/puppet/auth.conf
-puppet agent -v --server fbsd-srv02.servebeer.info --waitforcert 60 --test
-service puppet start
 
+puppet-install() {
+	pkg update
+	pkg install puppet
+}
+
+puppet-config() {
+	echo 'puppet_enable="YES"' >> /etc/rc.conf
+	echo 'puppet_flags="-v --listen --server fbsd-srv02.servebeer.info"' >> /etc/rc.conf
+	cp ~/puppet-bootstrap/configs/auth.conf /usr/local/etc/puppet/auth.conf
+}
+
+puppet-cert() {
+	puppet agent -v --server fbsd-srv02.servebeer.info --waitforcert 60 --test
+}
+
+puppet-start() {
+	service puppet start
+}
+
+if [ uid == 0 ]; then
+	puppet-install
+	puppet-config
+	puppet-cert
+	puppet-start
 else
-	echo "This must be run as root"
+	echo "This script must be run as root."
 fi
 exit 0
