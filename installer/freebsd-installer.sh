d65d7c9933e3a956aaa796619da4179dc6d7bbdd
Michael Beasley <youvegotmoxie@gmail.com>
2015-07-31 23:50:47 -0400

    split install.sh for modularity and to fix identifier issue

---
+++ installer/freebsd-installer.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+
+PATH=${PATH}
+
+# Define puppet.master.
+source configs/global/server.conf
+
+# Pkgng format only.
+puppet-install() {
+	pkg update
+	pkg install puppet
+}
+
+puppet-config() {
+	echo 'puppet_enable="YES"' >> /etc/rc.conf
+	echo 'puppet_flags="-v --listen --server ${SERVER}"' >> /etc/rc.conf
+	cp configs/freebsd/auth.conf /usr/local/etc/puppet/auth.conf
+}
+
+puppet-cert() {
+	puppet agent -v --server ${SERVER} --waitforcert ${TIMEOUT} --test
+}
+
+puppet-start() {
+	service puppet start
+}
+
+if [ "$(id -u)" != "0" ]; then
+
+	puppet-install
+	puppet-config
+	puppet-cert
+	puppet-start
+
+else
+	echo "This script must be run as root."
+fi
+exit 0
---
+++ installer/linux-installer.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+PATH=${PATH}
+
+# Define puppet.master.
+source configs/global/install.conf
+
+# Only supporting CentOS 7
+puppet-install() {
+	rpm -ivh https://yum.puppetlabs.com/el/7/products/x86_64/puppetlabs-release-7-10.noarch.rpm
+	yum install puppet
+}
+puppet-config() {
+	echo 'PUPPET_SERVER=${SERVER}' >> /etc/sysconfig/puppet
+	cp configs/linux/auth.conf /etc/puppet/auth.conf
+	echo 'server=${SERVER}' >> /etc/puppet/puppet.conf
+}
+puppet-cert() {
+	puppet agent -v --server ${SERVER} --waitforcert ${TIMEOUT} --test
+}
+puppet-start() {
+	systemctl enable puppet
+	systemctl start puppet
+}
+
+# Determine if root is running script.
+if [ "$(id -u)" != "0" ]; then
+
+	puppet-install
+	puppet-config
+	puppet-cert
+	puppet-start
+
+else
+	echo "This script must be run as root."
+fi
+exit 0
