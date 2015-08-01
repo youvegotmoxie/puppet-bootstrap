62f8985736c8831439139ba7ef49a259d9625d73
Michael Beasley <youvegotmoxie@gmail.com>
2015-08-01 00:30:32 -0400

    trivial

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@ PATH=${PATH}
 
 # Tested on Freebsd 10.1 and 10.2-BETA.
 freebsd-installer() {
-	sh installer/freebsd-installer.sh
+	./installer/freebsd-installer.sh
 }
 
 # Tested on CentOS 7.
 linux-installer() {
-	sh installer/linux-installer.sh
+	./installer/linux-installer.sh
 }
 
 # Determine OS as base system layout differs between FreeBSD and CentOS.  Not much
