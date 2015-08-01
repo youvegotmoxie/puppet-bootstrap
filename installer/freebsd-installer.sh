06f9a371721f416d6c66c266cef556ec8b16ea36
Michael Beasley <youvegotmoxie@gmail.com>
2015-08-01 00:51:07 -0400

    trivial

--- installer/freebsd-installer.sh
+++ installer/freebsd-installer.sh
@@ -14,7 +14,7 @@ puppet-install() {
 puppet-config() {
 	echo 'puppet_enable="YES"' >> /etc/rc.conf
 	echo 'puppet_flags="-v --listen --server ${SERVER}"' >> /etc/rc.conf
-	cp configs/freebsd/auth.conf /usr/local/etc/puppet/auth.conf
+	cp installer/configs/freebsd/auth.conf /usr/local/etc/puppet/auth.conf
 }
 
 puppet-cert() {
