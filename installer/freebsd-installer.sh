d01b8edab09c4faf928f3b50858dd8c925e00b95
Michael Beasley <youvegotmoxie@gmail.com>
2015-08-01 00:53:00 -0400

    rename: server.conf -> install.conf

--- installer/freebsd-installer.sh
+++ installer/freebsd-installer.sh
@@ -3,7 +3,7 @@
 PATH=${PATH}
 
 # Define puppet.master.
-source configs/global/server.conf
+source configs/global/install.conf
 
 # Pkgng format only.
 puppet-install() {
@@ -14,7 +14,7 @@ puppet-install() {
 puppet-config() {
 	echo 'puppet_enable="YES"' >> /etc/rc.conf
 	echo 'puppet_flags="-v --listen --server ${SERVER}"' >> /etc/rc.conf
-	cp installer/configs/freebsd/auth.conf /usr/local/etc/puppet/auth.conf
+	cp configs/freebsd/auth.conf /usr/local/etc/puppet/auth.conf
 }
 
 puppet-cert() {
